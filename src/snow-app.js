2ed6766f987c20b9c1c16556a01cc52e452ed931
XBigTK13X <xbigtk13x@gmail.com>
2025-09-20 22:55:52 -0500

    Fix scrolling cross platform problems

--- src/snow-app.js
+++ src/snow-app.js
@@ -1,17 +1,18 @@
-import { View, ScrollView, Platform } from 'react-native'
+import { View, ScrollView, Platform, Dimensions } from 'react-native'
 import { StyleContextProvider, useStyleContext } from './context/snow-style-context'
 import { FocusContextProvider } from './context/snow-focus-context'
-
-const fill = { flex: 1 }
 
 export function SnowApp(props) {
     const { SnowStyle } = useStyleContext(props)
     let rootInnerStyle = []
     if (Platform.OS === 'web') {
-        rootInnerStyle.push(fill)
+        rootInnerStyle.push({
+            height: Dimensions.get('window').height,
+            backgroundColor: 'black'
+        })
     }
     return (
-        <ScrollView style={fill} contentContainerStyle={rootInnerStyle}>
+        <ScrollView style={{ flex: 1, backgroundColor: 'black' }} contentContainerStyle={rootInnerStyle}>
             <StyleContextProvider snowStyle={props.snowStyle} snowConfig={props.snowConfig}>
                 <FocusContextProvider>
                     <View style={{ flex: 1, marginBottom: 50 }}>
